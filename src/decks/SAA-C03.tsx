80612d8b1f6dacdbf51a548cfb050ca51ea0fc4c
Tyler Kilburn <tylerkilburn@carfax.com>
2025-03-18 13:48:22 -0400

    Work in progress...

--- src/decks/SAA-C03.tsx
+++ src/decks/SAA-C03.tsx
@@ -1,5 +1,25 @@
 import { IFlashCard } from "../types"
 
+const GENERAL: IFlashCard[] = [
+  {
+    question: "What is High Availability",
+    answer: (
+      <>
+        Aims to <strong>ensure</strong> an agreed level of operational{" "}
+        <strong>performance</strong>, usually
+        <strong>uptime</strong>, for a{" "}
+        <strong>higher than normal period</strong>.
+        <ul>
+          <li>HA does not aim to stop failure.</li>
+          <li>Doesn't mean customers won't experience outages.</li>
+          <li>It is about maximizing a system's online time.</li>
+          <li>Aim is to minifify outages or the impact an outage has.</li>
+        </ul>
+      </>
+    ),
+  },
+]
+
 const CLOUDWATCH: IFlashCard[] = [
   {
     question: "What three main things does Cloudwatch Do?",
@@ -145,18 +165,19 @@ const IAM: IFlashCard[] = [
     question: "What is a Principle in the Context of IAM?",
     answer: (
       <>
-        An entity trying to access an AWS account.
+        An entity trying to access an AWS account via authentication.
         <ul>
-          <li>People</li>
-          <li>Computers</li>
-          <li>Services</li>
+          <li>Person</li>
+          <li>Computer</li>
+          <li>Service</li>
         </ul>
         Starts un-authed, must prove claim to becomes authed.
       </>
     ),
   },
   {
-    question: "What is the difference between authentication and authorization?",
+    question:
+      "What is the difference between authentication and authorization?",
     answer: <>--FILL--2.5.0</>,
   },
   {
@@ -171,8 +192,66 @@ const IAM: IFlashCard[] = [
     question: "How many groups can an IAM user belong to?",
     answer: <>10</>,
   },
+  {
+    question: "What are IAM groups?",
+    answer: (
+      <>
+        Are container for IAM users.
+        <ul>
+          <li>Can't log into them.</li>
+          <li>Have no creds of their own.</li>
+          <li>Used for organizing IAM users</li>
+        </ul>
+      </>
+    ),
+  },
+  {
+    question: "Is there an all users group in AWS",
+    answer: (
+      <>
+        No, not by default
+        <ul>
+          <li>You would have to implement yourself if you wanted this...</li>
+        </ul>
+      </>
+    ),
+  },
+  {
+    question: "What are IAM roles",
+    answer: (
+      <>
+        A type of identity that lives in an aws account.
+        <ul>
+          <li>Often used in a temporary capacity.</li>
+          <li>Represents a level of access.</li>
+        </ul>
+      </>
+    ),
+  },
+  {
+    question: "What kind of policies can be attached to an IAM Role?",
+    answer: (
+      <>
+        <ul>
+          <li>
+            The trust policy
+            <ul>
+              <li>Which identities can assume that Role.</li>
+              <li>
+                <strong></strong>:
+              </li>
+            </ul>
+          </li>
+          <li>The permissions policy</li>
+          <ul>
+            <li>What the Role is allowed to do.</li>
+          </ul>
+        </ul>
+      </>
+    ),
+  },
 ]
 
-const deck: IFlashCard[] = [...CLOUDWATCH, ...IAM]
+const deck: IFlashCard[] = [...GENERAL, ...CLOUDWATCH, ...IAM]
 
 export default deck
